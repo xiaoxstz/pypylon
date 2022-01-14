57b2692eced5d6f4788e4a2b89c2342b859b27fe
Tanzi <None>
2022-01-14 09:41:55 +0800

    grab_max_frame_rate: remove the code outside this file

--- samples/grab_max_frame_rate.py
+++ samples/grab_max_frame_rate.py
@@ -5,9 +5,6 @@
 from pypylon import genicam
 from pypylon import pylon
 
-from samples.configurationeventprinter import ConfigurationEventPrinter
-from samples.imageeventprinter import ImageEventPrinter
-
 import time
 
 
@@ -33,15 +30,6 @@ if __name__ == '__main__':
         # as all currently registered configuration handlers are removed by setting the registration mode to RegistrationMode_ReplaceAll.
         camera.RegisterConfiguration(pylon.SoftwareTriggerConfiguration(), pylon.RegistrationMode_ReplaceAll,
                                      pylon.Cleanup_Delete)
-
-        # For demonstration purposes only, add a sample configuration event handler to print out information
-        # about camera use.t
-        camera.RegisterConfiguration(ConfigurationEventPrinter(), pylon.RegistrationMode_Append, pylon.Cleanup_Delete)
-
-        # The image event printer serves as sample image processing.
-        # When using the grab loop thread provided by the Instant Camera object, an image event handler processing the grab
-        # results must be created and registered.
-        camera.RegisterImageEventHandler(ImageEventPrinter(), pylon.RegistrationMode_Append, pylon.Cleanup_Delete)
 
         # For demonstration purposes only, register another image event handler.
         camera.RegisterImageEventHandler(SampleImageEventHandler(), pylon.RegistrationMode_Append, pylon.Cleanup_Delete)
